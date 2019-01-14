ec7186a8c05ab5ff69762400b8f49031e5b9ba86
kojimain <kojimainjp@gmail.com>
2019-01-14 13:01:19 +0900

    サイト情報修正

--- components/header/Hero.vue
+++ components/header/Hero.vue
@@ -2,10 +2,15 @@
   <div class="hero is-info">
     <div class="hero-body has-text-centered">
       <h1 class="title">
-        <a href="/">
-          Cart Hill
-        </a>
+        <router-link
+          :to="{ name: 'index' }">
+          Cart
+        </router-link>
       </h1>
+      <p class="subtitle">
+        ショッピングサイトを模した練習サイトです。<br>
+        カートに追加した商品は丘の上から落とすことができますが、特に意味はありません。
+      </p>
     </div>
   </div>
 </template>
